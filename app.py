bc60c18cf2b2ada6237eb6626f4124e959723bce
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-06-10 16:58:19 -0300

    wip: validar que el nombre no esté vacío

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+app.secret_key = "super_secret"
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -8,6 +9,11 @@ def formulario():
     if request.method == "POST":
         nombre = request.form["nombre"]
         email = request.form["email"]
+
+        if len(nombre) < 1:
+            flash("El campo 'nombre' no puede estar vacío")
+            return redirect("/")
+
         return render_template("resultado.html", nombre=nombre, email=email)
     return render_template("formulario.html")
 
