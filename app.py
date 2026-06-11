0da5bb43235f2f924c727396cbc38cfd1c458475
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-06-10 23:15:00 -0300

    valida email y muestra errores de nombre de email

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import re
+
 from flask import Flask, flash, redirect, render_template, request
 
 app = Flask(__name__)
 app.secret_key = "super_secret"
+
+email_pattern = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -15,7 +19,11 @@ def formulario():
 
         if len(nombre) < 1:
             flash("El campo 'nombre' no puede estar vacío", "nombre_error")
-            return redirect("/")
+            return render_template("formulario.html", nombre=nombre, email=email)
+
+        if not re.fullmatch(email_pattern, email):
+            flash("Email con formato inválido", "email_error")
+            return render_template("formulario.html", nombre=nombre, email=email)
 
         return render_template("resultado.html", nombre=nombre, email=email)
     return render_template("formulario.html")
