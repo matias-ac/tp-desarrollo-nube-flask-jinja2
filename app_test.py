3697b5c83c25688ee03946223c01ff2c59e6ab0f
Matias Ariel Castro <matias.acastro1@gmail.com>
2026-06-12 10:15:54 -0300

    unit-green: agrega unit tests para validar existencia de input nombre y email

--- app_test.py
+++ app_test.py
@@ -1,3 +1,5 @@
+from bs4 import BeautifulSoup
+
 from app import app
 
 
@@ -11,3 +13,23 @@ def test_renderiza_el_formulario_con_titulo():
     tester = app.test_client()
     response = tester.get("/")
     assert "Formulario de registro" in response.text
+
+
+def test_formulario_contiene_input_nombre():
+    tester = app.test_client()
+    response = tester.get("/")
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    input_nombre = soup.find("input", attrs={"name": "nombre"})
+
+    assert input_nombre is not None
+
+
+def test_formulario_contiene_input_email():
+    tester = app.test_client()
+    response = tester.get("/")
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    input_email = soup.find("input", attrs={"name": "email"})
+
+    assert input_email is not None
